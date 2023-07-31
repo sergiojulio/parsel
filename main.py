07c948e7b5e00b2a50fbb984fd7ed4467bcade8d
sergiojulio <sergiojulio@gmail.com>
2023-07-30 20:03:49 -0400

    Fix

--- main.py
+++ main.py
@@ -6,11 +6,11 @@ import json
 HTMLFile = open("./bcv.html", "r")
 index = HTMLFile.read()
 
-#Web = requests.get('https://www.bcv.org.ve/')
-#index = Web.text
+Web = requests.get('https://www.bcv.org.ve/')
+index = Web.text
 
-html_selector = Selector(text=index)
-usd = html_selector.xpath('//div[@id="dolar"]/div/div/div/strong/text()').get()
+#html_selector = Selector(text=index)
+#usd = html_selector.xpath('//div[@id="dolar"]/div/div/div/strong/text()').get()
 
 
 usd = usd.strip()
